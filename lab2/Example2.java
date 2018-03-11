858a5ac0ead694c404e1218aa4a5a10f78141c5f
djeada <37275728+djeada@users.noreply.github.com>
2018-03-11 21:53:58 +0100

    Update Example2.java

--- lab2/Example2.java
+++ lab2/Example2.java
@@ -16,7 +16,7 @@ public class Example2 {
 		JButton przycisk1 = new JButton("Przycisk 1");
 		JButton przycisk2 = new JButton("Przycisk 2");
 		
-		//BorderLayout wyró¿nia 5 obszarów okna: górny (PAGE_START), dolny (PAGE_END), 
+		//BorderLayout wyroznia 5 obszarow okna: gorny (PAGE_START), dolny (PAGE_END), 
 		//lewy (LINE_START), prawy (LINE_END) i centralny (CENTER)
 		
 		frame.add(przycisk1, BorderLayout.PAGE_START);
@@ -28,7 +28,7 @@ public class Example2 {
 		JTextField field = new JTextField("A to pole tekstowe");
 		frame.add(field);
 		
-		//GridLayout jest bardzo wygodnym managerem, który dzieli okno na siatkê
+		//GridLayout jest bardzo wygodnym managerem, ktory dzieli okno na siatke
 		//u ustalonej liczbie kolumn i wierszy. 
 		
 		frame.setLayout(new GridLayout(2,2));
