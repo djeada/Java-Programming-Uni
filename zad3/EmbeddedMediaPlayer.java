8c2725bfb1f16912615950833f3b0dd7aede22dd
djeada <37275728+djeada@users.noreply.github.com>
2018-06-10 13:35:39 +0200

    Add files via upload

---
+++ zad3/EmbeddedMediaPlayer.java
@@ -0,0 +1,158 @@
+package zad3;
+
+import java.io.File;
+
+/*
+ * Copyright (c) 2012, 2014 Oracle and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  - Neither the name of Oracle nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+public class EmbeddedMediaPlayer extends Application {
+	
+    MediaPlayer mediaPlayer;
+    MediaControl mediaControl;
+    Group root;
+    BorderPane layout;
+
+    public void start(final Stage primaryStage) {
+    	
+    	//File menu
+        Menu fileMenu = new Menu("File");
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(e -> System.out.println("Create a new file..."));
+        fileMenu.getItems().add(newFile);
+        
+        MenuItem openFile = new MenuItem("Open...");
+        openFile.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				
+				FileChooser fc = new FileChooser();
+			    fc.setInitialDirectory(new File ("C:\\Users\\Adam\\Desktop"));
+			    fc.getExtensionFilters().addAll(
+			    new ExtensionFilter("Video Files", "*.mp4", "*.flv"));
+			    File selectedFile = fc.showOpenDialog(null);
+			            if(selectedFile != null) {
+			            	Media media = new Media (selectedFile.toURI().toString());
+					        mediaPlayer = new MediaPlayer(media);
+					        mediaControl = new MediaControl(mediaPlayer);
+					        layout.setCenter(mediaControl);
+			             }
+
+			    else {
+			        System.out.println("file is not valid");
+			    }	
+			}
+		});        
+        
+        fileMenu.getItems().add(openFile);
+        fileMenu.getItems().add(new MenuItem("Save..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Settings..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        fileMenu.getItems().add(new MenuItem("Exit..."));
+        
+        //Edit menu
+        Menu editMenu = new Menu("_Edit");
+        editMenu.getItems().add(new MenuItem("Cut"));
+        editMenu.getItems().add(new MenuItem("Copy"));
+        MenuItem paste = new MenuItem("Paste");
+        paste.setOnAction(e -> System.out.println("Pasting some crap"));
+        paste.setDisable(true);
+        editMenu.getItems().add(paste);
+
+        //Help menu
+        Menu helpMenu = new Menu("Help");
+        CheckMenuItem showLines = new CheckMenuItem("Show Line Numbers");
+        showLines.setOnAction(e -> {
+            if(showLines.isSelected())
+                System.out.println("Program will now display line numbers");
+            else
+                System.out.println("Hiding line number");
+        });
+        CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
+        autoSave.setSelected(true);
+        helpMenu.getItems().addAll(showLines, autoSave);
+
+        //Difficulty RadioMenuItems
+        Menu difficultyMenu = new Menu("Difficulty");
+        ToggleGroup difficultyToggle = new ToggleGroup();
+
+        RadioMenuItem easy = new RadioMenuItem("Easy");
+        RadioMenuItem medium = new RadioMenuItem("Medium");
+        RadioMenuItem hard = new RadioMenuItem("Hard");
+
+        easy.setToggleGroup(difficultyToggle);
+        medium.setToggleGroup(difficultyToggle);
+        hard.setToggleGroup(difficultyToggle);
+
+        difficultyMenu.getItems().addAll(easy, medium, hard);
+
+        //Main menu bar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu, difficultyMenu);
+
+        layout = new BorderPane();
+        layout.setTop(menuBar);
+    	
+        primaryStage.setTitle("Embedded Media Player");
+        root = new Group();
+        Scene scene = new Scene(root, 500, 300);
+
+        scene.setRoot(layout);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
